0a06842474d34f6396a30667d9caf493c9cffdcb
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-01-09 15:59:28 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,44 @@
+import playwright
+from playwright import async_playwright
+
+# Create an instance of Playwright
+async with async_playwright() as p:
+    # Get the current browser instance
+    browser = p.chromium
+    # Open a new context
+    context = await browser.new_context()
+    # Create a new page
+    page = await context.new_page()
+    # Go to TikTok
+    await page.goto('https://www.tiktok.com/trending')
+    # Select the first video from the trend list
+    first_video = await page.query_selector('div.trend-item-box > a')
+    # Get the link for the video
+    link = await first_video.getAttribute('href')
+    # Open the video page
+    await page.goto(link)
+    # Select the video element
+    video = await page.query_selector('video')
+    # Get the video source
+    video_src = await video.getAttribute('src')
+    # Download the video
+    await page.download_file(video_src, 'tiktok_video.mp4')
+
+# Upload the video to YouTube
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Set up the web driver
+driver = webdriver.Chrome()
+# Go to YouTube
+driver.get('https://www.youtube.com/')
+# Log in with email and password
+driver.find_element_by_name('identifier').send_keys('<EMAIL>')
+driver.find_element_by_name('password').send_keys('<PASSWORD>')
+driver.find_element(By.XPATH, '//*[@id="identifierNext"]/span/span').click()
+# Go to upload page
+driver.get('https://www.youtube.com/upload')
+# Upload the video
+driver.find_element_by_name('video').send_keys('tiktok_video.mp4')
+# Publish the video
+driver.find_element_by_name('publish').click()
